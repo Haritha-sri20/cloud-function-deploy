aa8ada9b6a528458a761c667d8d3a7733afafddc
Haritha-sri20 <harithahari2420@gmail.com>
2025-02-25 17:52:45 +0530

    Added the bucket code

--- cloud-storage/main.tf
+++ cloud-storage/main.tf
@@ -11,8 +11,3 @@ resource "google_storage_bucket" "cloud_function_bucket" {
   uniform_bucket_level_access = true
 }
 
-resource "google_storage_bucket_object" "cloud_function_manifest" {
-  name   = "cloudfunction.yaml"
-  bucket = google_storage_bucket.cloud_function_bucket.name
-  source = "cloudfunction.yaml"  # Ensure this file exists in your Terraform directory
-}
